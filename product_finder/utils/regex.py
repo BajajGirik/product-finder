11e0134ff799366ee998a5678adc4a39103e7d05
Girik Bajaj <girik.bajaj@leverageedu.com>
2025-02-06 06:59:49 +0530

    update regex utils

--- product_finder/utils/regex.py
+++ product_finder/utils/regex.py
@@ -1,8 +1,16 @@
 import re
-from constants import PRODUCT_PATTERNS
+from constants import PRODUCT_PATTERNS, NON_PRODUCT_PATTERNS
+from .url import URLUtils
 
 class RegexUtils:
     @staticmethod
-    def is_product_url(url):
-        return any(re.search(pattern, url) for pattern in PRODUCT_PATTERNS)
+    def get_product_url(url: str):
+        url = URLUtils.get_url_without_query_params(url)
 
+        # Handling amazon urls
+        refIndex = url.find("/ref")
+
+        if refIndex != -1:
+            url = url[:refIndex]
+
+        return url if any(re.search(pattern, url) for pattern in PRODUCT_PATTERNS) and not any(re.search(pattern, url) for pattern in NON_PRODUCT_PATTERNS) else ""
