c69daa5d6152d53817e8d886a11c4446361830fe
Girik Bajaj <girik.bajaj@leverageedu.com>
2025-02-06 07:00:01 +0530

    add pooling functionality in WebBrowser

--- product_finder/web_browser.py
+++ product_finder/web_browser.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Optional
+from typing import List
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -8,8 +8,6 @@ from playwright.async_api import async_playwright
 
 
 class WebBrowser:
-    semaphore = asyncio.Semaphore(15)
-
     def __init__(self) -> None:
         self.__last_scroll_height = 0
         self.__scroll_threshold = 10
@@ -17,15 +15,27 @@ class WebBrowser:
 
     async def quit(self):
         await self.__instance.stop()
-        WebBrowser.semaphore.release()
 
 
-    async def go_to_url(self, url, sleep_time = 1):
-        await WebBrowser.semaphore.acquire()
-
+    async def setup(self):
         self.__instance = await async_playwright().start()
-        browser = await self.__instance.chromium.launch()
+        browser = await self.__instance.chromium.launch(headless=False, args=[
+            "--disable-gpu",
+            "--disable-dev-shm-usage",  # Helps with memory in Docker
+            "--no-sandbox",  # Required for running in some environments
+            "--disable-background-networking",
+            "--disable-background-timer-throttling",
+            "--disable-backgrounding-occluded-windows",
+            "--disable-renderer-backgrounding",
+            "--disable-infobars",
+            "--disable-extensions",
+            "--blink-settings=imagesEnabled=false",  # Disables image loading
+            "--mute-audio"
+        ])
+        self.__page = await browser.new_page()
 
+
+    async def go_to_url(self, url, sleep_time = 2):
         # chrome_options = Options()
         # Run in background without spinning up GUI interface. (Improves efficiency)
         # chrome_options.add_argument("--headless")
@@ -47,8 +57,9 @@ class WebBrowser:
         # Would be better if web pages could be loaded asynchronously
         # self.__driver.get(url)
 
-        self.__page = await browser.new_page()
         await self.__page.goto(url)
+
+        await asyncio.sleep(sleep_time)
 
 
     async def __get_scroll_height(self):
@@ -85,3 +96,61 @@ class WebBrowser:
                 break
 
         return all_links
+
+
+class WebBrowserPoolItem:
+    def __init__(self, id: int, web_browser: WebBrowser) -> None:
+        self.__id = id
+        self.web_browser = web_browser
+        self.__is_available = True
+        
+    def get_id(self):
+        return self.__id
+
+    def is_available(self):
+        return self.__is_available
+
+    def set_available(self, is_available):
+        self.__is_available = is_available
+
+
+CONCURRENT_BROWSERS = 20
+
+class WebBrowserPool:
+    __concurrent_browsers = CONCURRENT_BROWSERS
+    __pool: List[WebBrowserPoolItem] = []
+    __semaphore = asyncio.Semaphore(CONCURRENT_BROWSERS)
+    __lock = asyncio.Lock()
+
+    @staticmethod
+    async def setup_single_browser():
+        web_browser = WebBrowser()
+        await web_browser.setup()
+        return web_browser
+
+
+    @staticmethod
+    async def get_instance():
+        async with WebBrowserPool.__lock:
+            if len(WebBrowserPool.__pool) == 0:
+                coros = [WebBrowserPool.setup_single_browser() for _ in range(WebBrowserPool.__concurrent_browsers)]
+
+                web_browsers = await asyncio.gather(*coros)
+
+                for web_browser in web_browsers:
+                    pool_item = WebBrowserPoolItem(len(WebBrowserPool.__pool), web_browser)
+                    WebBrowserPool.__pool.append(pool_item)
+
+        await WebBrowserPool.__semaphore.acquire()
+
+        for item in WebBrowserPool.__pool:
+            if item.is_available():
+                item.set_available(False)
+                return item
+
+        raise Exception("No available browsers")
+
+    @staticmethod
+    def release(id):
+        WebBrowserPool.__pool[id].set_available(True)
+        WebBrowserPool.__semaphore.release()
