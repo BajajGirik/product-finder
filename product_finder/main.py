2119c0ff5261f32780aa39783b26656986d2b04c
Girik Bajaj <girik.bajaj@leverageedu.com>
2025-02-06 09:48:37 +0530

    add logs for monitoring time

--- product_finder/main.py
+++ product_finder/main.py
@@ -1,6 +1,7 @@
 import asyncio
 from crawler import Crawler
 from web_browser import WebBrowserPool 
+from time import time
 
 async def crawl(domain):
     domain_crawler = Crawler(domain)
@@ -10,6 +11,9 @@ async def crawl(domain):
     print(f"{domain} urls saved")
 
 async def main():
+    start_time = time()
+    print("Starting Script")
+
     await WebBrowserPool.setup()
 
     domains = ["amazon.in", "flipkart.com"]
@@ -17,6 +21,11 @@ async def main():
     coros = [crawl(domain) for domain in domains]
 
     await asyncio.gather(*coros)
+
+    total_time = round(time() - start_time, 2)
+    print(
+        f"Total time taken to find products for {len(domains)} domains: {total_time} seconds"
+    )
 
 if __name__ == "__main__":
     asyncio.run(main())
